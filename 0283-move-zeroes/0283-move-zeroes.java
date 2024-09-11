202d213d17ae04433614f6f1b0f90ab485ba2409
riyas0511 <142474527+riyas0511@users.noreply.github.com>
2024-09-11 23:40:02 +0530

    Time: 2 ms (83.57%), Space: 46.2 MB (10.46%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public  static int[] moveZeroes(int[] a) 
+    {int j = -1;
+     int n=a.length;
+        //place the pointer j:
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+ if (j == -1) return a;
+      
+
+        //Move the pointers i and j
+        //and swap accordingly:
+        for (int i = j + 1; i < n; i++) {
+            if (a[i] != 0) {
+                //swap a[i] & a[j]:
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                j++;
+            }
+        }
+     return a;
+    
+    }
+}
