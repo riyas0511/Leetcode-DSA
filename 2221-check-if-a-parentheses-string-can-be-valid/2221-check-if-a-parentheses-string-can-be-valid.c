7dca15e986102b1374b5099cb06328928b3d1043
riyas0511 <142474527+riyas0511@users.noreply.github.com>
2025-01-12 23:34:05 +0530

    Time: 3 ms (100%), Space: 13.5 MB (100%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.c
@@ -0,0 +1,34 @@
+bool canBeValid(char *s, char *locked) {
+    int n = strlen(s);
+    if (n % 2 != 0)
+     return false;
+
+    int open = 0, close = 0, unlocked1 = 0, unlocked2 = 0;
+    for (int i = 0; i < n; i++) {
+        if (locked[i] == '0')
+         unlocked1++;
+
+        else if (s[i] == '(')
+         open++;
+
+        else if (s[i] == ')')
+         open--;
+
+        if (open + unlocked1 < 0)
+         return false;
+
+        int j = n - i - 1;
+        if (locked[j] == '0')
+         unlocked2++;
+
+        else if (s[j] == ')')
+         close++;
+
+        else if (s[j] == '(') 
+        close--;
+
+        if (close + unlocked2 < 0)
+         return false;
+    }
+    return true;
+}
