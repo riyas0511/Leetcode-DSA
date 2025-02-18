7d8908a642137ce25a115768713d4c60fd3c748d
riyas0511 <142474527+riyas0511@users.noreply.github.com>
2025-02-18 15:32:34 +0530

    Time: 8 ms (62.9%), Space: 23.1 MB (8.33%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+
+class Solution {
+public:
+      long long totalhrs(const vector<int>& arr, int hourly) {
+       long long totalM = 0;  // Use long long to prevent overflow
+        for (int i = 0; i < arr.size(); i++) {
+          int hourstoeat =  ceil( arr[i] / (double)hourly);
+          totalM += hourstoeat;
+
+      }
+       return totalM;
+    }
+
+
+        int maxelem(vector<int>& arr){
+            int max1=INT_MIN;
+            for(int i=0;i<arr.size();i++){
+                max1=max(max1,arr[i]);
+            }
+            return max1;
+        }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low =1 , high = maxelem(piles);
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            long long totalH = totalhrs(piles,mid);
+            if(totalH <= h){
+                high= mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+          return low;
+    }
+};
