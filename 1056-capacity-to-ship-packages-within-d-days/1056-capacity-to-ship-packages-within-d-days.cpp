82c5597ec6e6be6af13a3b82479e8ff9f14cb072
riyas0511 <142474527+riyas0511@users.noreply.github.com>
2025-02-18 21:12:31 +0530

    Time: 15 ms (36.73%), Space: 35.1 MB (35.94%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minCap(vector<int>& weights,int  min){
+        int n = weights.size();
+        int sum = 0;
+        int days= 1;
+        for(int i = 0; i< n; i++){
+            if(sum + weights[i] > min){
+                days += 1;  //move to the next day
+                sum = weights[i];       //load the weight
+            }else{
+            sum += weights[i];
+            }
+        }
+        return days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int sum = 0;
+        for(int i =0; i<weights.size();i++){
+            sum += weights[i];
+        }
+        int low =*max_element(weights.begin(), weights.end()) , high = sum;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int totaldays = minCap(weights, mid);
+            if(totaldays<=days){
+                high= mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
