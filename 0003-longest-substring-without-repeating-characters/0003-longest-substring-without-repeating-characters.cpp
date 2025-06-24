b0b8de1c525a7ebd5301ba112255fb46a799f757
riyas0511 <142474527+riyas0511@users.noreply.github.com>
2025-06-24 19:01:00 +0530

    Time: 0 ms (100%), Space: 11.6 MB (83.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,44 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int maxlen = 0;
-        unordered_set <int> set;
-        int l = 0;
-        int r = 0;
-        if(s.size() == 0){
-            return 0;
-        }
-        while(r < s.length()){
-            while(l < r && set.find(s[r])!= set.end()){
-               set.erase(s[l]);
-               l++;
-            }
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int maxlen = 0;
+//         unordered_set <int> set;
+//         int l = 0;
+//         int r = 0;
+//         if(s.size() == 0){
+//             return 0;
+//         }
+//         while(r < s.length()){
+//             while(l < r && set.find(s[r])!= set.end()){
+//                set.erase(s[l]);
+//                l++;
+//             }
             
-            set.insert(s[r]);
-            r++;
-            maxlen = max(maxlen , r - l);
-        }
-        return maxlen;
+//             set.insert(s[r]);
+//             r++;
+//             maxlen = max(maxlen , r - l);
+//         }
+//         return maxlen;
+//     }
+// };
+
+class Solution {
+ public:
+    int lengthOfLongestSubstring(string s) {
+        vector < int > mpp(256, -1);
+
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)
+          left = max(mpp[s[right]] + 1, left);
+
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
+      }
+      return len;
     }
 };
