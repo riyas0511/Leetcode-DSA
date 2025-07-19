395e0844e076943a888687870fef147b22a39542
riyas0511 <142474527+riyas0511@users.noreply.github.com>
2025-07-20 00:36:00 +0530

    Time: 9 ms (56.55%), Space: 11.9 MB (58.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,44 +1,24 @@
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         int maxlen = 0;
-//         unordered_set <int> set;
-//         int l = 0;
-//         int r = 0;
-//         if(s.size() == 0){
-//             return 0;
-//         }
-//         while(r < s.length()){
-//             while(l < r && set.find(s[r])!= set.end()){
-//                set.erase(s[l]);
-//                l++;
-//             }
-            
-//             set.insert(s[r]);
-//             r++;
-//             maxlen = max(maxlen , r - l);
-//         }
-//         return maxlen;
-//     }
-// };
-
 class Solution {
- public:
+public:
     int lengthOfLongestSubstring(string s) {
-        vector < int > mpp(256, -1);
-
-      int left = 0, right = 0;
-      int n = s.size();
-      int len = 0;
-      while (right < n) {
-        if (mpp[s[right]] != -1)
-          left = max(mpp[s[right]] + 1, left);
-
-        mpp[s[right]] = right;
-
-        len = max(len, right - left + 1);
-        right++;
-      }
-      return len;
+        int l = 0;
+        int r = 0;
+        unordered_map<char , int> freq;
+        int maxlen = INT_MIN;
+        if(s.empty()){
+            return 0;
+            
+        }
+        while(r < s.size()){
+            
+            while (freq[s[r]] > 0) {
+            freq[s[l]]--;
+            l++;
+        }
+            freq[s[r]]++;
+            maxlen = max(maxlen , r - l + 1);
+            r++;
+        }
+        return maxlen;
     }
 };
